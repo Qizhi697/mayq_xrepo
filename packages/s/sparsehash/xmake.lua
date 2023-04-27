cbc6977e7fcb7435ac016497b4b45842d81e041b
mayq <mayq697@163.com>
2023-04-27 07:06:13 +0000

    add sparsehash

--- packages/s/sparsehash/xmake.lua
+++ packages/s/sparsehash/xmake.lua
@@ -1,5 +1,12 @@
 package("sparsehash")
-    set_sourcedir(path.join("third_part", "sparsehash"))
+    -- set_sourcedir(path.join("third_part", "sparsehash"))
+    set_homepage("https://github.com/sparsehash/sparsehash")
+    set_description("mayq_sparsehash")
+    set_license("BSL")
+
+    add_urls("https://github.com/sparsehash/sparsehash/archive/refs/tags/sparsehash-2.0.4.tar.gz",
+             "https://github.com/sparsehash/sparsehash.git")
+    -- add_versions("0.10.3", "e9d4facd0c98b12b045de356c6c6c0c06d047d741fe61bde02ca9a68c82d7658")
     on_install("linux", function (package)
         os.vrun("./configure %s --prefix=\"%s\"", package:debug() and "--debug" or "", package:installdir())
         import("package.tools.make").install(package)
