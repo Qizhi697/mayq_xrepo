a1f2b61df01caced9a8ca4d1ae14ffa2635105d6
mayq <mayq697@163.com>
2023-04-27 07:55:34 +0000

    add sparse

--- packages/s/sparsehash/xmake.lua
+++ packages/s/sparsehash/xmake.lua
@@ -5,7 +5,7 @@ package("sparsehash")
 
     add_urls("https://github.com/sparsehash/sparsehash/archive/refs/tags/sparsehash-2.0.4.tar.gz",
              "https://github.com/sparsehash/sparsehash.git")
-    add_versions("0.10.3", "e9d4facd0c98b12b045de356c6c6c0c06d047d741fe61bde02ca9a68c82d7658")
+    -- add_versions("0.10.3", "e9d4facd0c98b12b045de356c6c6c0c06d047d741fe61bde02ca9a68c82d7658")
 
     on_install("linux", function (package)
         os.vrun("./configure %s --prefix=\"%s\"", package:debug() and "--debug" or "", package:installdir())
