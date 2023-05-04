39275e8a08cf1e0cf0149f61d3ff617660cb1066
mayq <mayq697@163.com>
2023-05-04 16:13:10 +0000

    fix folly

--- packages/f/folly/xmake.lua
+++ packages/f/folly/xmake.lua
@@ -20,7 +20,7 @@ package("folly")
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
     end
 
-    -- add_deps("cmake")1111111
+    add_deps("cmake")
     add_deps("boost", {configs = {context = true, filesystem = true, program_options = true, regex = true, system = true, thread = true}})
     add_deps("libevent", {configs = {openssl = true}})
     add_deps("double-conversion", "gflags", "glog", "zlib", "fmt", "libunwind")
